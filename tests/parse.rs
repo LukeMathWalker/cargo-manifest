0fd06a2aed594717aba2e4dd297aff28704b1d78
Luca Palmieri <lpalmieri@truelayer.com>
2020-11-29 12:46:01 +0000

    Reproduce bug.

--- tests/parse.rs
+++ tests/parse.rs
@@ -88,3 +88,44 @@ fn legacy() {
     let package = m.package.as_ref().unwrap();
     assert_eq!("foo", package.name);
 }
+
+/// Multi-word identifiers can be specified using both snake_case and kebab-case.
+/// This test ensures that the snake_case variant is handled correctly for `default-features`
+#[test]
+fn default_features_casing() {
+    let m = Manifest::from_str(
+        r#"
+[package]
+name = "foo"
+version = "1"
+
+[dependencies]
+rusoto_core = { version = "0.45.0", default_features=false, features=["rustls"] }
+"#,
+    )
+    .unwrap();
+
+    // `default-features`
+    let deps = m.dependencies.as_ref().unwrap();
+    let rusoto_core = deps.get("rusoto-core").unwrap().detail().unwrap();
+    assert!(rusoto_core.default_features.is_some());
+}
+
+/// This test ensures that the snake_case variant is handled correctly for `dev-dependencies`
+#[test]
+fn dev_dependencies_casing() {
+    let m = Manifest::from_str(
+        r#"
+[package]
+name = "foo"
+version = "1"
+
+[dev_dependencies]
+lazy_static = "1.4.0"
+"#,
+    )
+    .unwrap();
+
+    // `dev-dependencies`
+    assert!(m.dev_dependencies.is_some());
+}
