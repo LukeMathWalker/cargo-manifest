64dd0a5bf0be98f22f94a50ddbe5d95089dfb1d0
Tobias Bieniek <tobias@bieniek.cloud>
2023-09-28 11:06:45 +0200

    Use snapshot testing via `insta`
    ... instead of hardcoded assertions.
    
    This makes it much easier to update the expected values and at the same time allows us to assert on all of the struct fields without having to write dozens of assertions.

--- tests/parse.rs
+++ tests/parse.rs
@@ -1,5 +1,5 @@
 use cargo_manifest as lib;
-use cargo_manifest::{Manifest, MaybeInherited, Publish};
+use cargo_manifest::{Manifest, MaybeInherited};
 use std::fs::read;
 use std::str::FromStr;
 
@@ -21,92 +21,31 @@ fn own() {
 #[test]
 fn opt_level() {
     let m = Manifest::from_slice(&read("tests/opt_level.toml").unwrap()).unwrap();
-    let package = m.package.as_ref().unwrap();
-    assert_eq!("byteorder", package.name);
-    let profiles = m.profile.unwrap();
-    assert_eq!(
-        3,
-        profiles
-            .bench
-            .clone()
-            .unwrap()
-            .opt_level
-            .unwrap()
-            .as_integer()
-            .unwrap()
-    );
-    assert_eq!(
-        2,
-        profiles
-            .custom
-            .get("my-custom")
-            .cloned()
-            .unwrap()
-            .opt_level
-            .unwrap()
-            .as_integer()
-            .unwrap()
-    );
-    assert!(!m.lib.unwrap().bench);
-    assert_eq!(None, package.edition);
-    assert_eq!(1, m.patch.unwrap().len());
-    assert_eq!(
-        "unpacked",
-        profiles.bench.unwrap().split_debuginfo.unwrap().as_str()
-    );
+    insta::assert_debug_snapshot!(m);
 }
 
 #[test]
 fn autobin() {
     let m = Manifest::from_path("tests/autobin/Cargo.toml").expect("load autobin");
-    let package = m.package.as_ref().unwrap();
-    assert_eq!("auto-bin", package.name);
-    assert_eq!(
-        Some(MaybeInherited::Local(lib::Edition::E2018)),
-        package.edition
-    );
-    assert!(package.autobins);
-    assert!(m.lib.is_none());
-    assert_eq!(1, m.bin.as_ref().unwrap().len());
-    assert_eq!(Some("auto-bin"), m.bin.unwrap()[0].name.as_deref());
+    insta::assert_debug_snapshot!(m);
 }
 
 #[test]
 fn autolib() {
     let m = Manifest::from_path("tests/autolib/Cargo.toml").expect("load autolib");
-    let package = m.package.as_ref().unwrap();
-    assert_eq!("auto-lib", package.name);
-    assert_eq!(
-        Some(MaybeInherited::Local(Publish::Flag(false))),
-        package.publish
-    );
-    assert_eq!(None, package.edition);
-    assert!(package.autobins);
-    assert!(!package.autoexamples);
-    assert!(m.lib.is_some());
-    assert_eq!("auto_lib", m.lib.unwrap().name.unwrap());
-    assert_eq!(0, m.bin.unwrap().len());
+    insta::assert_debug_snapshot!(m);
 }
 
 #[test]
 fn autobuild() {
     let m = Manifest::from_path("tests/autobuild/Cargo.toml").expect("load autobuild");
-    let package = m.package.as_ref().unwrap();
-    assert_eq!(
-        Some(lib::StringOrBool::String("build.rs".into())),
-        package.build
-    );
+    insta::assert_debug_snapshot!(m);
 }
 
 #[test]
 fn metadata() {
     let m = Manifest::from_path("tests/metadata/Cargo.toml").expect("load metadata");
-    let package = m.package.as_ref().unwrap();
-    assert_eq!("metadata", package.name);
-    assert_eq!(
-        Some(lib::StringOrBool::String("foobar.rs".into())),
-        package.build
-    );
+    insta::assert_debug_snapshot!(m);
 }
 
 #[test]
@@ -137,11 +76,10 @@ fn legacy() {
                 "#,
     )
     .expect("parse old");
-    let package = m.package.as_ref().unwrap();
-    assert_eq!("foo", package.name);
+    insta::assert_debug_snapshot!(m);
+
     let m = Manifest::from_str("name = \"foo\"\nversion=\"1\"").expect("parse bare");
-    let package = m.package.as_ref().unwrap();
-    assert_eq!("foo", package.name);
+    insta::assert_debug_snapshot!(m);
 }
 
 // -- Multi-word identifiers can be specified using both snake_case and kebab-case --
@@ -160,10 +98,7 @@ rusoto_core = { version = "0.45.0", default_features=false, features=["rustls"] 
 "#,
     )
     .unwrap();
-
-    let deps = m.dependencies.as_ref().unwrap();
-    let rusoto_core = deps.get("rusoto_core").unwrap().detail().unwrap();
-    assert!(rusoto_core.default_features.is_some());
+    insta::assert_debug_snapshot!(m);
 }
 
 /// This test ensures that the snake_case variant is handled correctly for `build-dependencies`
@@ -180,8 +115,7 @@ lazy_static = "1.4.0"
 "#,
     )
     .unwrap();
-
-    assert!(m.build_dependencies.is_some());
+    insta::assert_debug_snapshot!(m);
 }
 
 /// This test ensures that the snake_case variant is handled correctly for `dev-dependencies`
@@ -198,8 +132,7 @@ lazy_static = "1.4.0"
 "#,
     )
     .unwrap();
-
-    assert!(m.dev_dependencies.is_some());
+    insta::assert_debug_snapshot!(m);
 }
 
 /// This test ensures that both the kebap-case and the snake_case variant is handled correctly for `proc-macro`
@@ -216,9 +149,7 @@ proc-macro = true
 "#,
     )
     .unwrap();
-
-    let lib = m.lib.as_ref().unwrap();
-    assert!(lib.proc_macro);
+    insta::assert_debug_snapshot!(m);
 
     let m = Manifest::from_str(
         r#"
@@ -231,9 +162,7 @@ proc_macro = true
 "#,
     )
     .unwrap();
-
-    let lib = m.lib.as_ref().unwrap();
-    assert!(lib.proc_macro);
+    insta::assert_debug_snapshot!(m);
 }
 
 /// We can work with package properties inherited from the workspace manifest.
@@ -250,17 +179,13 @@ documentation.workspace = true
 "#,
     )
     .unwrap();
-    let package = m.package.unwrap();
-    assert_eq!(MaybeInherited::inherited(), package.version);
-    assert_eq!(Some(MaybeInherited::inherited()), package.authors);
-    assert_eq!(Some(MaybeInherited::inherited()), package.description);
-    assert_eq!(Some(MaybeInherited::inherited()), package.documentation);
+    insta::assert_debug_snapshot!(m);
 }
 
 /// This test ensures that we correctly handle crate dependencies that are deferred to the top-level workspace.
 #[test]
 fn workspace_dependency() {
-    Manifest::from_str(
+    let m = Manifest::from_str(
         r#"
 [workspace]
 members = ["core"]
@@ -270,8 +195,9 @@ serde = { version = "1.0", features = [ "derive" ] }
 "#,
     )
     .unwrap();
+    insta::assert_debug_snapshot!(m);
 
-    Manifest::from_str(
+    let m = Manifest::from_str(
         r#"
 [package]
 name = "core"
@@ -283,4 +209,5 @@ tokio = { workspace = true, features = [ "rt-multi-thread" ] }
 "#,
     )
     .unwrap();
+    insta::assert_debug_snapshot!(m);
 }
