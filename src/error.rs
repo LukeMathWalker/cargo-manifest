289be918807bef4d461cad273473022ce81dbc66
Tobias Bieniek <tobias@bieniek.cloud>
2023-10-13 15:17:44 +0200

    Use `thiserror` crate to derive `Error` impls

--- src/error.rs
+++ src/error.rs
@@ -1,35 +1,15 @@
-use std::error::Error as StdErr;
-use std::fmt;
 use std::io;
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
-    Parse(toml::de::Error),
-    Io(io::Error),
-    Utf8(std::str::Utf8Error),
+    #[error(transparent)]
+    Parse(#[from] toml::de::Error),
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error(transparent)]
+    Utf8(#[from] std::str::Utf8Error),
+    #[error("{0}")]
     Other(String),
-}
-
-impl StdErr for Error {
-    fn source(&self) -> Option<&(dyn StdErr + 'static)> {
-        match *self {
-            Error::Parse(ref err) => Some(err),
-            Error::Io(ref err) => Some(err),
-            Error::Utf8(ref err) => Some(err),
-            Error::Other(_) => None,
-        }
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::Parse(ref err) => err.fmt(f),
-            Error::Io(ref err) => err.fmt(f),
-            Error::Utf8(ref err) => err.fmt(f),
-            Error::Other(msg) => f.write_str(msg),
-        }
-    }
 }
 
 impl Clone for Error {
@@ -40,23 +20,5 @@ impl Clone for Error {
             Error::Utf8(ref err) => Error::Utf8(*err),
             Error::Other(msg) => Error::Other(msg.clone()),
         }
-    }
-}
-
-impl From<toml::de::Error> for Error {
-    fn from(o: toml::de::Error) -> Self {
-        Error::Parse(o)
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(o: io::Error) -> Self {
-        Error::Io(o)
-    }
-}
-
-impl From<std::str::Utf8Error> for Error {
-    fn from(o: std::str::Utf8Error) -> Self {
-        Error::Utf8(o)
     }
 }
