93418b10e14c65dd735958701325910acd7092b0
Tobias Bieniek <tobias@bieniek.cloud>
2024-06-18 10:32:44 +0200

    AbstractFilesystem: Add doc comments (#40)
    Co-authored-by: Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>

--- src/afs.rs
+++ src/afs.rs
@@ -3,10 +3,20 @@ use std::fs::read_dir;
 use std::io;
 use std::path::Path;
 
+/// A trait for abstracting over filesystem operations.
+///
+/// This trait is primarily used for target auto-discovery in the
+/// [`complete_from_abstract_filesystem()`](crate::Manifest::complete_from_abstract_filesystem) method.
 pub trait AbstractFilesystem {
+    /// Returns a set of file and folder names in the given directory.
+    ///
+    /// This method should return a [std::io::ErrorKind::NotFound] error if the
+    /// directory does not exist.
     fn file_names_in<T: AsRef<Path>>(&self, rel_path: T) -> io::Result<BTreeSet<Box<str>>>;
 }
 
+/// A [AbstractFilesystem] implementation that reads from the actual filesystem
+/// within the given root path.
 pub struct Filesystem<'a> {
     path: &'a Path,
 }
