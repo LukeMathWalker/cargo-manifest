bdbb02e89ef6bf1db3649b2eaab5031466202988
Henk Oordt <hd@oordt.dev>
2025-01-15 13:57:12 +0100

    Fix impl Default for Manifest (#66)
    The current Default implementation is limited to `Manifest<_, Value>`, because the impl of `Default` only specifies the first type parameter

--- tests/serialize.rs
+++ tests/serialize.rs
@@ -2,8 +2,8 @@ use cargo_manifest::{Manifest, Package};
 
 #[test]
 fn basic() {
-    let manifest = Manifest {
-        package: Some(Package::<()>::new("foo".into(), "1.0.0".into())),
+    let manifest: Manifest<(), ()> = Manifest {
+        package: Some(Package::new("foo".into(), "1.0.0".into())),
         ..Default::default()
     };
 
